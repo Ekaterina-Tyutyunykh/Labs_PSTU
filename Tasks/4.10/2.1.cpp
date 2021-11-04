504b6ce81bb1da5caa7096695686e3200436c518
Ekaterina-Tyutyunykh <kate12367@mail.ru>
2021-11-04 11:46:37 +0500

    fix 2.1

--- Tasks/4.10/2.1.cpp
+++ Tasks/4.10/2.1.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, “Rus”);
+    setlocale(LC_ALL, "Rus");
     int F;
     double C;
-    cout << «Введите температуру в градусах по Фаренгейту : «;
+    cout << "Введите температуру в градусах по Фаренгейту : ";
     cin >> F;
     C = (5 * (F – 32)) / 9;
-    cout << «Тепмература в градусах по Цельцию : « << C;
+    cout << "Тепмература в градусах по Цельцию : " << C;
     return 0;
 }
