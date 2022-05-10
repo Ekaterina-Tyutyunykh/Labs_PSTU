fbcf4b53803e6dacc3784b5a6c74ad5b62e4b572
Ekaterina-Tyutyunykh <kate12367@mail.ru>
2022-05-10 13:31:44 +0500

    10.05 13:30 new window

--- Calculator/Calculator/Calculator/MyForm.h
+++ Calculator/Calculator/Calculator/MyForm.h
@@ -67,6 +67,20 @@ namespace Calculator {
 	private: System::Windows::Forms::TabControl^ tabControl1;
 	private: System::Windows::Forms::TabPage^ tabPage1;
 	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox9;
+	private: System::Windows::Forms::TextBox^ textBox11;
+	private: System::Windows::Forms::TextBox^ textBox12;
+	private: System::Windows::Forms::TextBox^ textBox13;
+	private: System::Windows::Forms::Label^ label12;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::TextBox^ textBox8;
+	private: System::Windows::Forms::TextBox^ textBox10;
+	private: System::Windows::Forms::Label^ label13;
+	private: System::Windows::Forms::Label^ label14;
+	private: System::Windows::Forms::Label^ label15;
 
 	protected:
 
@@ -112,8 +126,23 @@ namespace Calculator {
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox11 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox12 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox13 = (gcnew System::Windows::Forms::TextBox());
+			this->label12 = (gcnew System::Windows::Forms::Label());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox10 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label13 = (gcnew System::Windows::Forms::Label());
+			this->label14 = (gcnew System::Windows::Forms::Label());
+			this->label15 = (gcnew System::Windows::Forms::Label());
 			this->tabControl1->SuspendLayout();
 			this->tabPage1->SuspendLayout();
+			this->tabPage2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -194,7 +223,9 @@ namespace Calculator {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(136, 190);
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(179, 200);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(315, 51);
 			this->button1->TabIndex = 11;
@@ -314,7 +345,7 @@ namespace Calculator {
 			this->tabControl1->Location = System::Drawing::Point(3, 12);
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(716, 513);
+			this->tabControl1->Size = System::Drawing::Size(655, 513);
 			this->tabControl1->TabIndex = 21;
 			// 
 			// tabPage1
@@ -339,20 +370,193 @@ namespace Calculator {
 			this->tabPage1->Location = System::Drawing::Point(4, 25);
 			this->tabPage1->Name = L"tabPage1";
 			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage1->Size = System::Drawing::Size(708, 484);
+			this->tabPage1->Size = System::Drawing::Size(647, 484);
 			this->tabPage1->TabIndex = 0;
 			this->tabPage1->Text = L"Вычисление логарифма";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
 			// tabPage2
 			// 
+			this->tabPage2->Controls->Add(this->label15);
+			this->tabPage2->Controls->Add(this->textBox9);
+			this->tabPage2->Controls->Add(this->textBox11);
+			this->tabPage2->Controls->Add(this->textBox12);
+			this->tabPage2->Controls->Add(this->textBox13);
+			this->tabPage2->Controls->Add(this->label12);
+			this->tabPage2->Controls->Add(this->textBox6);
+			this->tabPage2->Controls->Add(this->textBox7);
+			this->tabPage2->Controls->Add(this->textBox8);
+			this->tabPage2->Controls->Add(this->textBox10);
+			this->tabPage2->Controls->Add(this->button2);
+			this->tabPage2->Controls->Add(this->comboBox1);
+			this->tabPage2->Controls->Add(this->label13);
+			this->tabPage2->Controls->Add(this->label14);
 			this->tabPage2->Location = System::Drawing::Point(4, 25);
 			this->tabPage2->Name = L"tabPage2";
 			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(708, 484);
+			this->tabPage2->Size = System::Drawing::Size(647, 484);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"Действия над логарифмами";
 			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// textBox9
+			// 
+			this->textBox9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox9->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox9->Location = System::Drawing::Point(493, 61);
+			this->textBox9->MaxLength = 3;
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(59, 49);
+			this->textBox9->TabIndex = 24;
+			// 
+			// textBox11
+			// 
+			this->textBox11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox11->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox11->Location = System::Drawing::Point(450, 97);
+			this->textBox11->MaxLength = 3;
+			this->textBox11->Name = L"textBox11";
+			this->textBox11->Size = System::Drawing::Size(37, 34);
+			this->textBox11->TabIndex = 27;
+			// 
+			// textBox12
+			// 
+			this->textBox12->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox12->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox12->Location = System::Drawing::Point(543, 21);
+			this->textBox12->MaxLength = 3;
+			this->textBox12->Name = L"textBox12";
+			this->textBox12->Size = System::Drawing::Size(37, 34);
+			this->textBox12->TabIndex = 26;
+			// 
+			// textBox13
+			// 
+			this->textBox13->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox13->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox13->Location = System::Drawing::Point(333, 50);
+			this->textBox13->MaxLength = 3;
+			this->textBox13->Name = L"textBox13";
+			this->textBox13->Size = System::Drawing::Size(57, 49);
+			this->textBox13->TabIndex = 25;
+			this->textBox13->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// label12
+			// 
+			this->label12->AutoSize = true;
+			this->label12->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label12->Location = System::Drawing::Point(387, 50);
+			this->label12->Name = L"label12";
+			this->label12->Size = System::Drawing::Size(80, 50);
+			this->label12->TabIndex = 23;
+			this->label12->Text = L"log";
+			// 
+			// textBox6
+			// 
+			this->textBox6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox6->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox6->Location = System::Drawing::Point(166, 50);
+			this->textBox6->MaxLength = 3;
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(59, 49);
+			this->textBox6->TabIndex = 17;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox7->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox7->Location = System::Drawing::Point(123, 86);
+			this->textBox7->MaxLength = 3;
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(37, 34);
+			this->textBox7->TabIndex = 22;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox8->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox8->Location = System::Drawing::Point(216, 10);
+			this->textBox8->MaxLength = 3;
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(37, 34);
+			this->textBox8->TabIndex = 21;
+			// 
+			// textBox10
+			// 
+			this->textBox10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox10->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->textBox10->Location = System::Drawing::Point(6, 39);
+			this->textBox10->MaxLength = 3;
+			this->textBox10->Name = L"textBox10";
+			this->textBox10->Size = System::Drawing::Size(57, 49);
+			this->textBox10->TabIndex = 19;
+			this->textBox10->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(166, 162);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(271, 46);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Вычислить";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"+", L"-", L"*", L"/" });
+			
+			this->comboBox1->Location = System::Drawing::Point(259, 50);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(63, 50);
+			this->comboBox1->TabIndex = 0;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::comboBox1_SelectedIndexChanged);
+			// 
+			// label13
+			// 
+			this->label13->AutoSize = true;
+			this->label13->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label13->Location = System::Drawing::Point(60, 39);
+			this->label13->Name = L"label13";
+			this->label13->Size = System::Drawing::Size(80, 50);
+			this->label13->TabIndex = 16;
+			this->label13->Text = L"log";
+			// 
+			// label14
+			// 
+			this->label14->AutoSize = true;
+			this->label14->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label14->Location = System::Drawing::Point(576, 50);
+			this->label14->Name = L"label14";
+			this->label14->Size = System::Drawing::Size(65, 69);
+			this->label14->TabIndex = 28;
+			this->label14->Text = L"=";
+			// 
+			// label15
+			// 
+			this->label15->AutoSize = true;
+			this->label15->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label15->Location = System::Drawing::Point(273, 232);
+			this->label15->Name = L"label15";
+			this->label15->Size = System::Drawing::Size(65, 69);
+			this->label15->TabIndex = 29;
+			this->label15->Text = L"=";
 			// 
 			// MyForm
 			// 
@@ -360,7 +564,7 @@ namespace Calculator {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::HighlightText;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
-			this->ClientSize = System::Drawing::Size(727, 524);
+			this->ClientSize = System::Drawing::Size(660, 524);
 			this->Controls->Add(this->tabControl1);
 			this->ForeColor = System::Drawing::SystemColors::ControlText;
 			this->Name = L"MyForm";
@@ -369,6 +573,8 @@ namespace Calculator {
 			this->tabControl1->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
 			this->tabPage1->PerformLayout();
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
@@ -549,6 +755,21 @@ namespace Calculator {
 private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 }
 private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	
+	
+	
+
+
+
+
+
+
+}
+private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	/*int den = (int)comboBox1->SelectedValue;
+	label15->Text = den.ToString();*/
 }
 };
 };
